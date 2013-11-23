c4914527fb28051d98659eb1bdae310561aa034a
mvalvik <morten@valvik.dk>
2013-11-23 14:20:27 +0100

    Project created

---
+++ Panorama/src/com/example/panorama/PanoramaActivity.java
@@ -0,0 +1,22 @@
+package com.example.panorama;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PanoramaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panorama);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_panorama, menu);
+        return true;
+    }
+    
+}
